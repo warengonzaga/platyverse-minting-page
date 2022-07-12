d3e64c54377b44130caf5b9b3ec47db25f8a2f09
Waren Gonzaga <github@warengonzaga.com>
2022-07-12 10:23:52 +0000

    Updates

---
+++ _next/static/lzacGljoA20m-LF_rJWnd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0cf6f46e7860f734.css","static/chunks/pages/index-1061260b1d0b653c.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/pGIZ5KMsBQviO-6ODIO86/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/96abfd37426605e6.css","static/chunks/pages/index-1061260b1d0b653c.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
